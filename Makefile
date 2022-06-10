957f4a4390e73e10cc2e71d84f2161de684ebe2d
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-09 18:41:08 -0700

    Version 2.0 - Changes Styles and Colors

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 generator.x: main.o
-	g++ main.o -o generator.x 
+	g++ -std=c++11 main.o -o generator.x 
 
 main.o: main.cpp
-	g++ main.cpp -o main.o -c
+	g++ -std=c++11 main.cpp -o main.o -c
 
 clean:
 	rm -f *.o GENERATEDREADME.md
+
+delete: 
+	rm -f *.x *.o GENERATEDREADME.md
--- main.cpp
+++ main.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 
 int main() {
+    string repo;
     string repoName;
-    cout << "Whose repo are you generating badges for: " << endl;
+    string repoTitle;
+    string colorString;
+    string styleString;
+    int color;
+    int style;
+    vector<string> colors = {"", "color=red", "color=blue", "color=purple", "color=green", "color=yellow", "color=pink", "color=orange", "color=brown", "color=grey"};
+    vector<string> styles = {"\"/>", "style=for-the-badge\"/>"};
+
+    system("clear");
+    cout << "Whose repo are you generating badges for: ";
     cin >> repoName;
 
-    string repoTitle;
-    cout << "Which repo are you generating badges for: " << endl;
+    system("clear");
+    cout << "Which repo are you generating badges for: ";
     cin >> repoTitle;
     
-    string repo = repoName + "/" + repoTitle;
+    repo = repoName + "/" + repoTitle;
+
+    system("clear");
+    cout << "Which color would you like the badge to be: " << endl;
+    cout << "1 - Github Original" << endl;
+    cout << "2 - For The Badge" << endl;
+    cin >> style;
+    styleString = styles.at(style - 1);
+
+    system("clear");
+    cout << "Which color would you like the badge to be: " << endl;
+    cout << "0 - Github Original" << endl;
+    cout << "1 - Red" << endl;
+    cout << "2 - Blue" << endl;
+    cout << "3 - Purple" << endl;
+    cout << "4 - Green" << endl;
+    cout << "5 - Yellow" << endl;
+    cout << "6 - Pink" << endl;
+    cout << "7 - Orange" << endl;
+    cout << "8 - Brown" << endl;
+    cout << "9 - Grey" << endl;
+    cin >> color;
+    colorString = colors.at(color);
 
     string line1 = "<img alt=\"GitHub Language Count\" src=\"https://img.shields.io/github/languages/count/";
     string line2 = "<img alt=\"\" src=\"https://img.shields.io/github/repo-size/";
@@ -20,13 +54,12 @@ int main() {
     string line4 ="<img alt=\"GitHub Closed Issues\" src=\"https://img.shields.io/github/issues-closed/";
     string line5 = "<img alt=\"GitHub Contributors\" src=\"https://img.shields.io/github/contributors/";
     string line6 = "<img alt=\"GitHub Commit Activity (Week)\" src=\"https://img.shields.io/github/commit-activity/w/";
-    string style = "?style=for-the-badge\"/>";
-    string badge1 = line1 + repo + style;
-    string badge2 = line2 + repo + style;
-    string badge3 = line3 + repo + style;
-    string badge4 = line4 + repo + style;
-    string badge5 = line5 + repo + style;
-    string badge6 = line6 + repo + style;
+    string badge1 = line1 + repo + "?" + colorString + "&" + styleString;
+    string badge2 = line2 + repo + "?" + colorString + "&" + styleString;
+    string badge3 = line3 + repo + "?" + colorString + "&" + styleString;
+    string badge4 = line4 + repo + "?" + colorString + "&" + styleString;
+    string badge5 = line5 + repo + "?" + colorString + "&" + styleString;
+    string badge6 = line6 + repo + "?" + colorString + "&" + styleString;
 
     //Open File
     ofstream File("GENERATEDREADME.md");
@@ -45,5 +78,6 @@ int main() {
     // Close the file
     File.close();
 
+    system("clear");
     cout << "README.md for " + repoTitle + " has been generated!" << endl;
 }
